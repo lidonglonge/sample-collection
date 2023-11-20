4547bd29d3ac549ca4c63464ef261a98953fbc40
东龙 <lidonglonge@163.com>
2023-11-20 16:53:28 +0800

    feat: 自述

---
+++ string-operation-demo/src/main/java/com/dl/str/StringOperationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dl.str;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringOperationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringOperationDemoApplication.class, args);
+	}
+
+}
---
+++ string-operation-demo/src/test/java/com/dl/str/StringOperationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dl.str;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringOperationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
